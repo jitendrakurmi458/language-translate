a8760e8a65b176cad3649de36026dde17065be47
uuklanger <uuklanger@bac6e38a-952c-0410-a5f2-a1104aab0ee2>
2007-03-30 04:28:52 +0000

    a little code cleanup and and comments

--- languagetranslate/src/net/kenware/langtrans/GoogleTranslater.java
+++ languagetranslate/src/net/kenware/langtrans/GoogleTranslater.java
@@ -2,6 +2,20 @@
  * GoogleTranslater.java
  *
  * Created on March 23, 2007, 10:01 PM
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 package net.kenware.langtrans;
@@ -26,18 +40,43 @@ import org.apache.commons.httpclient.methods.GetMethod;
 import au.id.jericho.lib.html.*;
 
 /**
+ * A module for taking an input string along with country codes (source/target)
+ * and converts it to the target language.  It uses Google's Language Tool
+ * site to do the actual translating.  
+ * 
+ * This program was done to simplify and speed up my ability to translate 
+ * quick message.
  *
- * @author klanger
+ * @author uuklanger
  */
 public class GoogleTranslater {
+    // =============================================================
+    //  Constructors and Init
+    // =============================================================
     
+    // <editor-fold defaultstate="collapsed" desc="Constructors and Init">
     /**
      * Creates a new instance of GoogleTranslater
      */
     public GoogleTranslater() {
         return;
     }
+    // </editor-fold>
     
+    // =============================================================
+    //  Public Methods
+    // =============================================================
+    
+    // <editor-fold defaultstate="collapsed" desc="Public Methods">
+    /**
+     * translates source string (sin) into a response using the source/target
+     * languages.
+     *
+     * @param sin String in
+     * @param sourcelang source language (ISO3166 code)
+     * @param targetlang target language (ISO3166 code)
+     * @return returns a text string in the target language
+     */
     public String urlTranslate(String sin, String sourcelang, String targetlang) {
         String response = "";
         String url = null;
@@ -72,7 +111,20 @@ public class GoogleTranslater {
         
         return(response);
     }
+    // </editor-fold>
     
+    // =============================================================
+    //  Private Methods
+    // =============================================================
+    
+    // <editor-fold defaultstate="collapsed" desc="Private Methods">
+    /**
+     * Using jericho libraries, this will extract the response
+     * text from the HTML blob returned from google Language Tools.
+     *
+     * @param html HTML blob
+     * @return Response text
+     */
     private String extractResponse(String html) {
         String response = null;
         String attribute = null;
@@ -113,8 +165,15 @@ public class GoogleTranslater {
         
         return(response);
     }
+    // </editor-fold>
     
+    // =============================================================
+    //  TEST MAIN
+    // =============================================================
+    
+    // <editor-fold defaultstate="collapsed" desc="Test Main">
     /**
+     * Test main for development
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -126,4 +185,5 @@ public class GoogleTranslater {
         
         System.exit(0);
     }
+    // </editor-fold>
 } // end-class
